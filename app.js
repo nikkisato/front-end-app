c131744635ac294ef6cf6c176ef8d93fbbe4cb29
Nikki Sato <54095031+nikkisato@users.noreply.github.com>
2023-01-30 22:09:59 -0800

    inital commit

--- app.js
+++ app.js
@@ -1,34 +1,15 @@
-// async function getData(req, res) {
-//   await fetch("http://localhost:8000", {
-//     method: "GET",
-//     headers: {
-//       "Content-Type": "application/json",
-//       "Access-Control-Allow-Origin": "http://localhost:8000",
-//       "Access-Control-Allow-Credentials": "true",
-//       mode: "cors",
-//     },
-//   })
-//     .then((response) => {
-//       console.log(response);
-//       return response.json();
-//     })
-//     .catch((err) => {
-//       console.log(err);
-//     });
-// }
-
-// getData();
-
-fetch(
-  "http://localhost:8000/",
-  { mode: "no-cors" },
-  {
-    method: "GET",
-    headers: {
-      "Access-Control-Allow-Origin": "*",
-      "Access-Control-Allow-Methods": "GET, DELETE, HEAD, OPTIONS",
-    },
-  }
-);
-
-// fetch("https://cors-demo.glitch.me/allow-cors", { mode: "cors" });
+async function getData(req, res) {
+  await fetch("http://localhost:8000/", {
+    origin: "*",
+    methods: "GET,HEAD,PUT,PATCH,POST,DELETE",
+    preflightContinue: false,
+  })
+    .then((res) => {
+      console.log(res);
+      res.json();
+    })
+    .catch((err) => {
+      console.log(err);
+    });
+}
+getData();
