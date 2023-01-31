be016f746ee927e7afb99a5cfe764f20fa8969d9
Nikki Sato <54095031+nikkisato@users.noreply.github.com>
2023-01-30 19:44:21 -0800

    added async function app.js

--- app.js
+++ app.js
@@ -1,9 +1,34 @@
-fetch("http://localhost:8000/", {
-  method: "GET",
-})
-  .then((response) => {
-    return response.json();
-  })
-  .catch((err) => {
-    console.log(err);
-  });
+// async function getData(req, res) {
+//   await fetch("http://localhost:8000", {
+//     method: "GET",
+//     headers: {
+//       "Content-Type": "application/json",
+//       "Access-Control-Allow-Origin": "http://localhost:8000",
+//       "Access-Control-Allow-Credentials": "true",
+//       mode: "cors",
+//     },
+//   })
+//     .then((response) => {
+//       console.log(response);
+//       return response.json();
+//     })
+//     .catch((err) => {
+//       console.log(err);
+//     });
+// }
+
+// getData();
+
+fetch(
+  "http://localhost:8000/",
+  { mode: "no-cors" },
+  {
+    method: "GET",
+    headers: {
+      "Access-Control-Allow-Origin": "*",
+      "Access-Control-Allow-Methods": "GET, DELETE, HEAD, OPTIONS",
+    },
+  }
+);
+
+// fetch("https://cors-demo.glitch.me/allow-cors", { mode: "cors" });
